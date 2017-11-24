c13cd5d1cff63421edc3960622e3f2c7add3f132
Ganesh Navale <navaleganesh2011@gmail.com>
2017-11-24 22:59:53 +0530

    updated angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule }    from '@angular/forms';
+
+
 import { MaterializeModule } from 'angular2-materialize';
 import { Angular2TokenService } from 'angular2-token';
 
@@ -12,8 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MaterializeModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    MaterializeModule
   ],
   providers: [ Angular2TokenService ],	
   bootstrap: [AppComponent]
