6340894115dde3883c9fb3016f088d8e7f6e692c
Ganesh Navale <navaleganesh2011@gmail.com>
2017-11-25 00:06:33 +0530

    added empty modal for login/register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 import { MaterializeModule } from 'angular2-materialize';
@@ -9,10 +9,16 @@ import { Angular2TokenService } from 'angular2-token';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent,
+    AuthDialogComponent
   ],
   imports: [
     BrowserModule,
